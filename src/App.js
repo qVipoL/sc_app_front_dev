5abd68e27497c2323f6f522b007c2eb41142aae8
alfam <alfamedor283@gmail.com>
2019-10-08 13:24:46 +0300

    eeqe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
+import signup from './pages/signup';
+import login from './pages/login';
+import home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Out App</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={home} />
+          <Route path='/login' component={login} />
+          <Route path='/signup' component={signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
