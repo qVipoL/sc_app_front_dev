ccba199999049a8bd44c79a4ee88e462fc0079ac
alfam <alfamedor283@gmail.com>
2019-10-08 19:12:58 +0300

    jjjj

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+
+// Pages
 import signup from './pages/signup';
 import login from './pages/login';
 import home from './pages/home';
 
+// Components
+import Navbar from './components/Navbar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#388e3c',
+    },
+    secondary: {
+      main: '#ff5722',
+    },
+  },
+  typography: {
+    useNextVariants: true
+  }
+})
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path='/' component={home} />
-          <Route path='/login' component={login} />
-          <Route path='/signup' component={signup} />
-        </Switch>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={home} />
+              <Route exact path='/login' component={login} />
+              <Route exact path='/signup' component={signup} />
+              <Route component={home} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Post from '../components/Post';
+import axios from 'axios'
+import Grid from '@material-ui/core/Grid'
+
+
+
+export class home extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            posts: null
+        }
+    }
+    componentDidMount(){
+        axios.get('/posts')
+        .then(res => {
+            this.setState({
+                posts: res.data
+            })
+            console.log(this.state)
+        })
+        .catch(err => console.log(err))
+    }
+    render() {
+        let recentPostsMarkup = this.state.posts
+         ? (this.state.posts.map(post => <Post key={post.createdAt} post={post} />))
+         : <p>loading ...</p>
+        return (
+            <Grid container spacing={10}>
+                <Grid item sm={8} sx={12}>
+                    <h1>{recentPostsMarkup}</h1>
+                </Grid>
+                <Grid item sm={4} sx={12}>
+                    <p>Profile</p>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default home
