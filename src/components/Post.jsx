81637519d4fb800030e13c99d389b8dc8cc20f69
alfam <alfamedor283@gmail.com>
2019-10-09 16:57:43 +0300

    jewjjew

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import Link from 'react-router-dom/Link'
+import { Link } from 'react-router-dom'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
 // MUI
 import withStyles from '@material-ui/core/styles/withStyles'
 import Card from '@material-ui/core/Card'
@@ -26,13 +29,14 @@ const styles = {
 
 export class Post extends Component {
     render() {
+        dayjs.extend(relativeTime)
         const { classes, post: { body, createdAt, userImage, userHandle, postId, likeCount, commentCount } } = this.props
         return (
             <Card className={classes.card}>
                 <CardMedia image={userImage} title="Profile image" className={classes.image}/>
                 <CardContent className={classes.content}>
                     <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                    <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography>
                 </CardContent>
             </Card>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,140 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import AppIcon from '../images/icon.png'
+import axios from 'axios'
+
+// MUI
+import withStyles from '@material-ui/core/styles/withStyles'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    },
+    formIcon: {
+        maxWidth: '25%',
+        opacity: 0.3,
+        marginTop: '10px' 
+    },
+    textField: {
+        marginBottom: '10px'
+    },
+    button: {
+        marginTop: '20px' 
+    },
+    pageTitle: {
+        marginBottom: '5px'
+    },
+    customError: {
+        color: 'red',
+        fontSize: '0.8rem'
+    }
+}
 
 export class login extends Component {
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {}
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+
+        this.setState({
+            loading: true
+        })
+
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        axios.post('/login', userData)
+        .then(res => {
+            console.log(res.data)
+            this.setState({loading: false})
+            this.props.history.push('/')
+        })
+        .catch(err => {
+            this.setState({
+                errors: err.response.data,
+                loading: false
+            })
+        })
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
+        const { classes } = this.props
+        const { errors, loading } = this.state
+
         return (
-            <div>
-                <h1>Login Page</h1>
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm />
+                <Grid item sm>
+                    <img src={AppIcon} alt="icon" className={classes.formIcon}/>
+                    <Typography variant="h4" className={classes.pageTitle}>
+                        <p>Login</p>
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField
+                        id="email"
+                        name="email" 
+                        type="email" 
+                        label="Email" 
+                        className={classes.textField}
+                        helperText={errors.email} 
+                        error={errors.email ? true : false} 
+                        value={this.state.email} 
+                        onChange={this.handleChange} 
+                        fullWidth />
+
+                        <TextField 
+                        id="password" 
+                        name="password" 
+                        type="password" 
+                        label="Password" 
+                        className={classes.textField}
+                        helperText={errors.password} 
+                        error={errors.email ? true : false} 
+                        value={this.state.password} 
+                        onChange={this.handleChange} 
+                        fullWidth />
+
+                        {errors.general && (
+                            <Typography variant="body2" className={classes.customError}>
+                                {errors.general}
+                            </Typography>
+                        )}
+
+                        <Button type="submit" variant="contained" color="primary" className={classes.button}>Log In</Button>
+                    </form>
+                </Grid>
+                <Grid item sm />
+            </Grid>
         )
     }
 }
 
-export default login
+login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login)
